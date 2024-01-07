c9ebfc377f1b05872685d7a0474c521c69450f50
vojdam <vojdam@email.cz>
2024-01-07 20:48:59 +0100

    added batch files for updating/reseting db

---
+++ extractor_app/commands.py
@@ -0,0 +1,18 @@
+import click
+
+from . import metadata_extractor
+from . import db
+
+
+@click.command("update-db")
+def update_database():
+    """Updates the database"""
+    me = metadata_extractor.MetadataExtractor("extractor_app\config.ini")
+    me.loop_through_instances()
+
+
+@click.command("init-db")
+def init_db_command() -> None:
+    """Clear the existing data and create new tables."""
+    db.init_db()
+    click.echo("Initialized the database.")
