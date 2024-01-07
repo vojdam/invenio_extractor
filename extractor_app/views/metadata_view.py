32e9204cf4828738f395e3f4d27726a72ad42700
vojdam <vojdam@email.cz>
2024-01-07 19:24:03 +0100

    added metadata viewing, base page functional

---
+++ extractor_app/views/metadata_view.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template
+
+from .. import db
+
+bp = Blueprint("metadata_view", __name__)
+
+
+@bp.get("/")
+def home():
+    """base.html homepage that lists all files"""
+    database = db.get_db()
+
+    session_list = database.execute("SELECT * FROM SpecimenSession").fetchall()
+
+    max_folder_id = database.execute(
+        "SELECT MAX(FolderID) FROM SpecimenSession"
+    ).fetchall()
+
+    return render_template(
+        "base.html",
+        session_list=session_list,
+        max_folder_id=max_folder_id,
+    )
+
+
+@bp.get("/<int:item_id>")
+def metadata_view_page(item_id: int):
+    database = db.get_db()
+    item_SpecimenSession = database.execute(
+        f"SELECT * FROM SpecimenSession WHERE SpecimenSessionID = {item_id}"
+    ).fetchall()
+    item_SpecimenDescriptionSequence = database.execute(
+        f"SELECT * FROM SpecimenDescriptionSequence WHERE SpecimenDescriptionSequenceID = {item_id}"
+    ).fetchall()
+    item_PrimaryAnatomicStructureSequence = database.execute(
+        f"SELECT * FROM PrimaryAnatomicStructureSequence WHERE PrimaryAnatomicStructureSequenceID = {item_id}"
+    ).fetchall()
+
+    return render_template(
+        "item_view.html",
+        item_SpecimenSession=item_SpecimenSession,
+        item_SpecimenDescriptionSequence=item_SpecimenDescriptionSequence,
+        item_PrimaryAnatomicStructureSequence=item_PrimaryAnatomicStructureSequence,
+    )
