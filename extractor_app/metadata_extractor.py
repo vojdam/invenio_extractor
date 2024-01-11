82930a57d8d768b87ccf0a5ef45f99c4e4550855
vojdam <vojdam@email.cz>
2024-01-11 21:36:57 +0100

    added image thumbnail view

--- extractor_app/metadata_extractor.py
+++ extractor_app/metadata_extractor.py
@@ -23,7 +23,7 @@ class MetadataExtractor:
             SamplesPerPixel, PhotometricInterpretation, Rows, Columns, BitsAllocated, 
             BitsStored, HighBit, PixelRepresentation, LossyImageCompression, ContainerIdentifier, 
             IssuerOfTheContainerIdentifierSequence, ContainerTypeCodeSequence, 
-            AcquisitionContextSequence, FolderID, ImageID)
+            AcquisitionContextSequence, FolderID, ImageID, ImageFileName)
             VALUES
             (:SpecificCharacterSet, :ImageType, :SOPClassUID, 
             :SOPInstanceUID, :StudyDate, :SeriesDate, :StudyTime, :SeriesTime, :AccessionNumber, 
@@ -36,7 +36,7 @@ class MetadataExtractor:
             :SamplesPerPixel, :PhotometricInterpretation, :Rows, :Columns, :BitsAllocated, 
             :BitsStored, :HighBit, :PixelRepresentation, :LossyImageCompression, 
             :ContainerIdentifier, :IssuerOfTheContainerIdentifierSequence, 
-            :ContainerTypeCodeSequence, :AcquisitionContextSequence, :FolderID, :ImageID)"""
+            :ContainerTypeCodeSequence, :AcquisitionContextSequence, :FolderID, :ImageID, :ImageFileName)"""
         self.sql_string_SpecimenDescriptionSequence = """INSERT INTO SpecimenDescriptionSequence
             (SpecimenIdentifier, SpecimenUID, IssuerOfTheSpecimenIdentifierSequence, 
             SpecimenShortDescription, SpecimenPreparationSequence, FolderID) 
@@ -65,40 +65,9 @@ class MetadataExtractor:
                 dict_meta = dcm_file.to_json_dict()
                 del dict_meta["7FE00010"]
             dict_meta["ImageID"] = file[:7]
+            dict_meta["ImageFileName"] = file
             dict_meta_list.append(dict_meta)
         return dict_meta_list
-
-    # def _extract_values_from_full_meta(self, full_meta: dict) -> dict:
-    #     """Extracts truncated metadata from the full metadata dict
-    #     HARDCODED!!!
-    #     """
-    #     # TODO:
-    #     # add ContainerIdentifier, SpecimenDescription
-    #     truncated_meta = {
-    #         "Slozka vzorku": full_meta["0020000D"].get("Value", " ")[0],
-    #         "Nazev vzorku": full_meta["00100020"].get("Value", " ")[0],
-    #         "Jmeno pacienta": self._handle_structured_values(
-    #             full_meta["00100010"].get("Value", " "), "Alphabetic"
-    #         ),
-    #         "Pohlavi pacienta": full_meta["00100040"].get("Value", " ")[0],
-    #         "Datum narozeni pacienta": self._format_date(
-    #             full_meta["00100030"].get("Value", " ")[0]
-    #         ),
-    #         "Datum odberu": self._format_date(
-    #             full_meta["00080020"].get("Value", " ")[0]
-    #         ),
-    #     }
-    #     return truncated_meta
-
-    # def _handle_structured_values(self, value_attribute: list, desired_key: str) -> str:
-    #     """Handles Values of a tag that contain a dictionary type"""
-    #     if value_attribute == " ":
-    #         return " "
-    #     for item in value_attribute:
-    #         if type(item) == dict:
-    #             return item[desired_key]
-    #         elif type(item) == dict:
-    #             return " "
 
     def format_date(self, date: str) -> str:
         """Formats date from yyyymmdd to yyyy-mm-dd"""
@@ -109,7 +78,7 @@ class MetadataExtractor:
         all_keys = full_dict.keys()
         new_all_keys = []
         for key in all_keys:
-            if key == "ImageID":
+            if key == "ImageID" or key == "ImageFileName":
                 new_all_keys.append(key)
                 continue
             new_all_keys.append(dictionary_keyword(key))
@@ -138,7 +107,7 @@ class MetadataExtractor:
                 full_meta = self._get_metadata(folder)
             except NotADirectoryError:
                 continue
-            logging.info(f"Zahajeno ukladani metadat ze slozky {folder}")
+            logging.info(f"Starting metadata extraction out of folder: {folder}")
             for dictionary in full_meta:
                 translated_dictionary = self._translate_codes(dictionary)
 
@@ -159,7 +128,7 @@ class MetadataExtractor:
                     folder_id,
                 )
             folder_id += 1
-        logging.info("Vsechna metadata jsou ulozena!")
+        logging.info("Metadata saved successfully!")
 
     def _check_for_changes(self) -> list | int:
         """Compares written image folders and available image folders"""
@@ -182,11 +151,11 @@ class MetadataExtractor:
     def write_to_database(
         self, meta_dict: dict, sql_string: str, folder_id: int
     ) -> None:
-        """WIP function for writing into database"""
+        """Write to a SQL database"""
         database = sqlite3.connect(self.database_path)
         new_values = []
         for key, value in meta_dict.items():
-            if key == "ImageID":
+            if key == "ImageID" or key == "ImageFileName":
                 new_values.append(value)
                 continue
             new_values.append(str(value.get("Value", "   "))[2:-2])
---
+++ extractor_app/views/image_view.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template
+
+import base64
+from io import BytesIO
+
+from matplotlib.figure import Figure
+
+import pydicom
+
+from .. import db
+
+
+bp = Blueprint("image_view", __name__)
+
+
+# consider something else than mpl
+@bp.get("/images/<folder>/<image_filename>")
+def image_viewer(folder: str, image_filename: str):
+    path = f"instance\\images\\{folder}\\{image_filename}"
+    dataset = pydicom.dcmread(path)
+    fig = Figure()
+    ax = fig.subplots()
+    ax.imshow(dataset.pixel_array)
+    ax.set_axis_off()
+    buffer = BytesIO()
+    fig.savefig(buffer, format="png", bbox_inches="tight")
+    data = base64.b64encode(buffer.getbuffer()).decode("ascii")
+    return render_template("image_view.html", image=f"data:image/png;base64,{data}")
